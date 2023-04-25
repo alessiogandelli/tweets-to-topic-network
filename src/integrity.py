3d41017ff840a82bca0cbc8e036111fb4f79dce0
alessiogandelli <alessiogandelli99@gmail.com>
2023-04-25 15:13:17 +0200

    prepara data

---
+++ src/integrity.py
@@ -0,0 +1,46 @@
+
+#%%
+import pandas as pd
+import numpy as np
+import os
+import re
+import igraph
+from igraph import Graph
+
+
+
+path = '/Volumes/boot420/Users/data/climate_network/cop22/'
+file_user = 'users_cop22.csv'
+file_tweets = 'tweets_cop22.csv'
+# %%
+df_user = pd.read_csv(os.path.join(path,file_user), index_col=0)
+# unnamed: 0 is id 
+
+id_to_users = df_user['username'].to_dict()
+users_to_id = {v:k for k,v in id_to_users.items()}
+
+# %%
+
+#df_tweets = pd.read_pickle(os.path.join(path,'tweets_cop21.pkl'))
+
+# cleaning 
+df_tweets = df_tweets[~df_tweets['author'].isna()]
+df_tweets = df_tweets[df_tweets['author'].apply(lambda x: x.isdigit())]
+
+# remove all author that  dont contains numbers 
+
+df_tweets['mentions']= df_tweets['text'].apply(lambda x: re.findall(r'@(\w+)', str(x)))
+df_tweets['mention_id'] = df_tweets['mentions'].apply(lambda x: [users_to_id.get(i) for i in x])
+df_tweets['author'] = df_tweets['author'].astype(int)
+df_tweets['author_name'] = df_tweets['author'].apply(lambda x: id_to_users.get(int(x)))
+
+
+
+# %%
+# remove all author that  dont contains numbers using isdigit
+author_name = df_tweets['author_name'].isna().sum()/ len(df_tweets)
+
+mentions = df_tweets['mention_id'].to_list()
+flatten = [item for sublist in mentions for item in sublist]
+
+none_count = flatten.count(None) / len(flatten)
--- src/network.py
+++ src/network.py
@@ -3,50 +3,52 @@ import pandas as pd
 import numpy as np
 import os
 import re
-import tweepy
-from dotenv import load_dotenv
+import igraph
+from igraph import Graph
+from networkx.algorithms import bipartite
+import networkx as nx
 
-load_dotenv()
-
-
-
-client = tweepy.Client(bearer_token=os.getenv('TOKEN_TWITTER'))
-
-#%%
-path = '/Users/alessiogandelli/dev/internship/data'
+path = '/Volumes/boot420/Users/data/climate_network/cop22/'
+file_user = 'users_cop22.csv'
+file_tweets = 'tweets_cop22.csv'
 
 
+# create new bipartite graph 
 
-cop27 = pd.read_csv(os.path.join(path, 'cop27_tw.csv'),  sep='\t',lineterminator='\n')
 
-#%%
+g = nx.Graph()
+g.add_nodes_from(sample_df['author_name'].unique(), bipartite=0)
+g.add_nodes_from(sample_df.index, bipartite=1)
+
+# list of tuples between author_nname and index 
+edges = list(zip(sample_df['author_name'], sample_df.index))
+ref_edges = list(zip(sample_df.index, sample_df['referenced_id']))
+# remove all none values
+ref_edges = [i for i in ref_edges if i[1] is not None]
+g.add_edges_from(edges, weight = 10, )
+g.add_edges_from(ref_edges, weight = 1)
+# add attribute bipartite to all nides without it 
+for i in g.nodes:
+    if 'bipartite' not in g.nodes[i]:
+        g.nodes[i]['bipartite'] = 1
+
+# #remove all nodes 0 that have out degree 1
+g.remove_nodes_from([n for n in g if g.degree(n) < 15 and g.nodes[n]['bipartite']==0])
+# # remove nodes 1 that have in degree 0
+g.remove_nodes_from([n for n in g if g.degree(n) == 0 ])
 
 # %%
-cop27 = cop27[cop27['lang'] == 'en'] # only english
-cop27 = cop27[cop27['has_media'] == 'N'] #has media no 
-cop27 = cop27[~cop27['text'].str.startswith('RT')]# remove retweets ( if starts euth RT)
-cop27 = cop27.drop_duplicates(subset=['text'])# remove text duplicates
-cop27['timestamp'] = pd.to_datetime(cop27[['year', 'month', 'day', 'h', 'm', 's']])
-cop27 = cop27.sort_values(by='timestamp')
-cop27 = cop27.drop(columns=['year', 'day', 'h', 'm', 's'])# remove year month day h m s
-
-
-cop27=cop27[~cop27['text'].str.contains('\t')] # remove text that contains tab due to importing problems
-# %%
-
-cop27['mentions'] = cop27['text'].str.findall(r'@(\w+)') # find mentions
-cop27['mentions_id'] = cop27['mentions'].apply(lambda x: [client.get_user(username=i).data.id for i in x]) # find mentions id
+# plot graph 
+import matplotlib.pyplot as plt
+color = [0 if g.nodes[i]['bipartite']==0 else 1 for i in g.nodes]
+pos = nx.bipartite_layout(g,sample_df.index)
+pos = nx.spring_layout(g, k = 0.07)
+#pos = nx.multipartite_layout(g, subset_key='bipartite')
+nx.draw(g, with_labels=False, pos = pos, node_size = 4, node_color = color, width = 0.1)
+plt.show()
 
 # %%
-def get_user_id(list_mentions):
-    users = []
-    
-    for username in list_mentions:
-
-        if len(list_mentions) > 0:
-            try:
-                users.append(client.get_user(username=username).data.id)
-            except:
-                pass
-    return users
 # %%
+
+# get the biggest degree
+max_degree = max(g.degree())
