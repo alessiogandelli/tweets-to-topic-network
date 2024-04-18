fa66aa82bcdcef9a0deacbd217cae4589f39e768
alessiogandelli <alessiogandelli99@gmail.com>
2024-04-18 19:47:43 +0200

    include tweets with attachments

--- tweets_to_topic_network/data.py
+++ tweets_to_topic_network/data.py
@@ -100,32 +100,29 @@ class Data_processor:
     def _load_tweets_json(self, users):
         print('looking into tweets json file')
         tweets = {}
-        attachments = 0
         with jsonlines.open(self.file_tweets) as reader: # open file
             for obj in reader:
-                if obj.get('attachments') is  None : # avoid tweets with attachments (should be analyzed with )
-                    tweets[obj.get('id', 0)] = {'author': obj['author_id'], 
-                                                'author_name': users.get(obj['author_id'], {}).get('username'),
-                                                'text': obj.get('text', ''), 
-                                                'date': obj.get('created_at', ''),
-                                                'lang':obj.get('lang', ''),
-                                                # 'reply_count': obj.get('public_metrics', {}).get('reply_count', 0), 
-                                                # 'retweet_count': obj.get('public_metrics', {}).get('retweet_count', 0), 
-                                                # 'like_count': obj.get('public_metrics', {}).get('like_count', 0), 
-                                                # 'quote_count': obj.get('public_metrics', {}).get('quote_count', 0),
-                                                #'impression_count': obj['public_metrics']['impression_count'],
-                                                'conversation_id': obj.get('conversation_id', None),
-                                                'referenced_type': obj.get('referenced_tweets', [{}])[0].get('type', None),
-                                                'referenced_id': obj.get('referenced_tweets', [{}])[0].get('id', None),
-                                                'mentions_name': [ann.get('username', '') for ann in obj.get('entities',  {}).get('mentions', [])],
-                                                'mentions_id': [ann.get('id', '') for ann in obj.get('entities',  {}).get('mentions', [])],
-                                                'cop':  self.n_cop
-                                                # 'context_annotations': [ann.get('entity').get('name') for ann in obj.get('context_annotations', [])]
-                                                }
-                else:
-                    attachments+=1
+                tweets[obj.get('id', 0)] = {'author': obj['author_id'], 
+                                            'author_name': users.get(obj['author_id'], {}).get('username'),
+                                            'text': obj.get('text', ''), 
+                                            'date': obj.get('created_at', ''),
+                                            'lang':obj.get('lang', ''),
+                                            # 'reply_count': obj.get('public_metrics', {}).get('reply_count', 0), 
+                                            # 'retweet_count': obj.get('public_metrics', {}).get('retweet_count', 0), 
+                                            # 'like_count': obj.get('public_metrics', {}).get('like_count', 0), 
+                                            # 'quote_count': obj.get('public_metrics', {}).get('quote_count', 0),
+                                            #'impression_count': obj['public_metrics']['impression_count'],
+                                            'conversation_id': obj.get('conversation_id', None),
+                                            'referenced_type': obj.get('referenced_tweets', [{}])[0].get('type', None),
+                                            'referenced_id': obj.get('referenced_tweets', [{}])[0].get('id', None),
+                                            'mentions_name': [ann.get('username', '') for ann in obj.get('entities',  {}).get('mentions', [])],
+                                            'mentions_id': [ann.get('id', '') for ann in obj.get('entities',  {}).get('mentions', [])],
+                                            'attachments': obj.get('attachments', None),
+                                            'cop':  self.n_cop
+                                            # 'context_annotations': [ann.get('entity').get('name') for ann in obj.get('context_annotations', [])]
+                                            }
 
-            print('discarded ',attachments ,'tweets with attachments')
+
 
         df_tweets = pd.DataFrame(tweets).T
         return df_tweets
