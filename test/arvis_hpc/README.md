2654efab92a821564b7bf5eb9c17f103cfe620a6
alessiogandelli <alessiogandelli99@gmail.com>
2024-05-09 19:06:31 +0200

    avoid problems with openai api key

---
+++ test/arvis_hpc/README.md
@@ -0,0 +1,4 @@
+1) create a def file for the plugin recipe.def
+2) with scp send the files to the cluster 
+
+3) apptainer build container.sif recipe.def # build the container and create the sif file
---
+++ test/arvis_hpc/arvis_tm.py
@@ -0,0 +1,39 @@
+#%%
+import pandas as pd
+from tweets_to_topic_network.topic import Topic_modeler
+
+import sys
+
+file_tweets = '/Users/alessiogandelli/data/cop23/cache/data/tweets_cop23.pkl'
+
+file_tweets = sys.argv[1]
+
+print(file_tweets)
+
+df = pd.read_pickle(file_tweets)
+
+df = df[df['referenced_type'].isna()]
+
+# %%
+
+# tm = Topic_modeler(df, name = 'cop23', embedder_name='all-MiniLM-L6-v2', path_cache = '/Users/alessiogandelli/data/cop23/cache')
+# df_labeled = tm.get_topics()
+
+# # %%
+# tm = Topic_modeler(df, name = 'cop23', embedder_name='paraphrase-MiniLM-L3-v2', path_cache = '/Users/alessiogandelli/data/cop23/cache')
+# df_labeled = tm.get_topics()
+
+# # %%
+
+# tm = Topic_modeler(df, name = 'cop23', embedder_name='text-embedding-3-small', path_cache = '/Users/alessiogandelli/data/cop23/cache')
+# df_labeled = tm.get_topics()
+
+
+# # %%
+# # tm.docs is a list of strings get the lenght of each 
+
+# len_docs = [len(doc) for doc in tm.docs]
+# %%
+
+
+# %%
